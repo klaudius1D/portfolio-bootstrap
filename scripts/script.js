6dc1d23979fc8ed5e8254290e42bbd49b2f1e216
Klaude Dungca <klaude.dungca@gmail.com>
2019-06-17 14:45:08 -0600

    fixed scripts

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,8 @@
 //Get the current year
 $('#year').text(new Date().getFullYear());
 
-//slider configuration
-$('.carousel').carousel({
-    interval: 6000,
-    pause: 'hover'
 
-});
-
-// Video Play - from stackOverflow
-$(function () {
-    // Auto play modal video
-    $(".video").click(function () {
-        var theModal = $(this).data("target"),
-            videoSRC = $(this).attr("data-video"),
-            videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
-        $(theModal + ' iframe').attr('src', videoSRCauto);
-        $(theModal + ' button.close').click(function () {
-            $(theModal + ' iframe').attr('src', videoSRC);
-        });
-    });
-});
-
-//lightbox plugin by ashleydw https://ashleydw.github.io/lightbox/
-$(document).on('click', '[data-toggle="lightbox"]', function (event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
-});
-
-$('.slider').slick({
-    infinite: true,
-    slideToShow: 1,
-    slideToScroll: 1
+//hide on collapse
+$('.port-item').click(function () {
+    $('.collapse').collapse('hide');
 });
